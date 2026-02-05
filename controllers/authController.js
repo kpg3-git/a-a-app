18a462c11c1f6b950f45b1ba6a73b46c3e978e10
Gayathri <kpgayathri33@gmail.com>
2026-02-05 11:16:22 +0530

    Fixed auth-me

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,13 +49,19 @@ export const loginUser = async (req, res) => {
 };
 
 // CURRENT USER - GET
-export const getMe = (req, res) => {
-  res.json({
-    message: "Authenticated user",
-    userId: req.user.id
-  });
-};
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
 
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 // ALL USERS - GET
 export const getAllUsers = async (req, res) => {
